56425ee021b4d88488ce7930f1fd3dbcc6b67254
KarenLaines <144739875+KarenLaines@users.noreply.github.com>
2025-08-05 10:57:37 -0600

    feat(vite): initialize vanilla JavaScript project with Vite
    - Set up base project structure using Vite vanilla template
    
    - Includes default counter component, styles, and development server

---
+++ pictures/clase05/index.js
@@ -0,0 +1,8 @@
+let value = 1 ==="1" ? "iguales": "No son iguales";
+console.log(value)
+
+
+const PASSWORD = ProcessingInstruction.env.PASSWORD ? ProcessingInstruction.env.PASSWORD : null;
+
+// variables de MediaElementAudioSourceNode, no deben ser expuestas, deben ser encriptadas o aseguradas
+// es una variable que nosotros vamos a inyectar en el sistema, pra guardar una variable podemos guardarla como asd.
