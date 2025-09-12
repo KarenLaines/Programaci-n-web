6f7132a57824d45f43c75aa1180df6538e273c1d
KarenLaines <144739875+KarenLaines@users.noreply.github.com>
2025-09-12 17:08:11 -0600

    feat: register models in admin

--- myproject/catalog/admin.py
+++ myproject/catalog/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Product, ProductReview, ProductImage, ProductInventory
+
+admin.site.register(Product)
+admin.site.register(ProductReview)
+admin.site.register(ProductImage)
+admin.site.register(ProductInventory)
